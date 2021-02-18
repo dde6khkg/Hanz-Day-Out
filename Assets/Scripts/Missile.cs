041883fd119251eecb42fa70390aa9acc125538d
JmWisecarver <35155170+JmWisecarver@users.noreply.github.com>
2021-02-18 04:33:24 -0800

    Last commit failed to add new assets

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    //public GameObject hitEffect;
+    public Rigidbody2D rb;
+    public int damage = 10;
+    public float aoe_radius = 10;
+    PlayerMovement player;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        //Destroy(effect, 2f);
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<Enemy>().takeDamage(damage);
+            collision.gameObject.GetComponent<Enemy>().rb.velocity = new Vector2(0, 0);
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            player.takeDamage(damage);
+            player.rb.velocity = new Vector2(0, 0);
+        }
+        MissileExplode();
+        
+
+        Destroy(gameObject);
+    }
+
+
+    void MissileExplode()
+    {
+        Vector2 Pos = transform.position;
+        Debug.Log(transform.position);
+        
+    }
+}
