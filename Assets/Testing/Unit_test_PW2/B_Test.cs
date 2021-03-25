43e2e739343693656df377c9fd9c9f15b8c6dfea
JmWisecarver <35155170+JmWisecarver@users.noreply.github.com>
2021-03-24 23:03:53 -0700

    Fixed B tests from parker

--- Assets/Testing/Unit_test_PW2/B_Test.cs
+++ Assets/Testing/Unit_test_PW2/B_Test.cs
@@ -19,12 +19,8 @@ namespace Tests
         {
             var gameObject = new GameObject();
             var GameManager = gameObject.AddComponent<GameManager>();
-<<<<<<< HEAD
             var door = new GameObject();
             
-=======
-
->>>>>>> 3496a1f7891c6d2173305378a3fd9ef823f578bf
             int i, eNum = GameManager.enemiesLeft;
 
             for(i = 2; i < 0; i--)
