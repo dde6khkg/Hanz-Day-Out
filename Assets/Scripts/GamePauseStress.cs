89f4ff5bd62c988198ed9aaa7527cac808358232
JmWisecarver <35155170+JmWisecarver@users.noreply.github.com>
2021-03-24 18:04:36 -0700

    Added stress tests and a unit test space

---
+++ Assets/Scripts/GamePauseStress.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePauseStress : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+            Time.timeScale = 0f;
+    }
+}
